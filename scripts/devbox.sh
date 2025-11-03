e9e92bd9e76b61ad855840587528c0fd8823af17
Mario Lopez <mariolopjr@users.noreply.github.com>
2025-11-02 23:47:51 -0500

    fix paths

--- scripts/devbox.sh
+++ scripts/devbox.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./distrobox-shims.sh
+/tmp/distrobox-shims.sh
 
 # Update the container and install packages
 apk update && apk upgrade
-grep -v '^#' ./devbox.packages | xargs apk add
+grep -v '^#' /tmp/devbox.packages | xargs apk add
 
