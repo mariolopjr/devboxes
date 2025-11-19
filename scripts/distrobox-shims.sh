b28a198224e82af6bafbd6bf9bce40c8eff3c006
Mario <mariolopjr@users.noreply.github.com>
2025-11-18 21:35:39 -0500

    add systemctl shim

--- scripts/distrobox-shims.sh
+++ scripts/distrobox-shims.sh
@@ -5,5 +5,6 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/systemctl
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
 
