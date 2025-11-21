071478c1b1f2d083a079d6e401ec66ab271a0400
Mario <mariolopjr@users.noreply.github.com>
2025-11-21 13:50:41 -0500

    added usage and wezterm

--- build_files/scripts/distrobox-shims.sh
+++ build_files/scripts/distrobox-shims.sh
@@ -7,4 +7,5 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/systemctl
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/wezterm
 
