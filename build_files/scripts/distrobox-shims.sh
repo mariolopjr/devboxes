b38bc4c1b1eb9962de4671fe490d8da313223409
Mario <mariolopjr@users.noreply.github.com>
2025-11-22 16:59:28 -0500

    add restic shim

--- build_files/scripts/distrobox-shims.sh
+++ build_files/scripts/distrobox-shims.sh
@@ -5,6 +5,7 @@ ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/dnf
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
+ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/restic
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/systemctl
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/transactional-update
